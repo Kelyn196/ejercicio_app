e016737ada78b886f47c29ad7e781f14930be266
Kelyn196 <ariasleonela360@gmail.com>
2025-03-01 08:59:10 -0600

    Ejercicio clase

---
+++ lib/pantallas/contador.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  @override
+  int clickcontador = 0;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Contador')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Cantidad de clicks'),
+            Text(
+              '$clickcontador',
+              style: TextStyle(fontSize: 150, fontWeight: FontWeight.w100),
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: Row(
+        mainAxisAlignment:
+            MainAxisAlignment.end, // Alinea los botones a la derecha
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickcontador++;
+              });
+            },
+            child: Icon(Icons.plus_one),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickcontador--;
+              });
+            },
+            child: Icon(Icons.exposure_minus_1),
+          ),
+        ],
+      ),
+      //floatingActionButton: FloatingActionButton(
+      //onPressed: () {
+      //clickcontador++;
+      //setState(() {});
+      //},
+      //child: Icon(Icons.plus_one),
+      //),
+    );
+  }
+}
